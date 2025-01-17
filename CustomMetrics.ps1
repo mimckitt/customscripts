cc90f7d5ac96d15d5b4ddd48069a53bd72e55b11
Micah McKittrick <32313503+mimckitt@users.noreply.github.com>
2025-01-17 11:00:34 -0800

    Update CustomMetrics.ps1

--- CustomMetrics.ps1
+++ CustomMetrics.ps1
@@ -1,31 +1,38 @@
 New-NetFirewallRule -DisplayName 'HTTP(S) Inbound' -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8000')
-                $Hso = New-Object Net.HttpListener
-                $Hso.Prefixes.Add('http://localhost:8000/')
-                $Hso.Start()
-                function GenerateResponseJson()
-                {
-                    $appHealthState = "Healthy"
-                    $hashTable = @{
-                        'ApplicationHealthState' = $appHealthState
-                        'CustomMetrics' = @{
-                            'RollingUpgrade' = @{
-                                'PhaseOrderingNumber' = 1
-                                'SkipUpgrade' = "false"
-                            }
-                        }
-                    } 
-                    $hashTable.CustomMetrics = ($hashTable.CustomMetrics | ConvertTo-Json)
-                    return ($hashTable | ConvertTo-Json)
-                }
-                While($Hso.IsListening)
-                {
-                    $context = $Hso.GetContext()
-                    $response = $context.Response
-                    $response.StatusCode = 200
-                    $response.ContentType = 'application/json'
-                    $responseJson = GenerateResponseJson
-                    $responseBytes = [System.Text.Encoding]::UTF8.GetBytes($responseJson)
-                    $response.OutputStream.Write($responseBytes, 0, $responseBytes.Length)
-                    $response.Close()
-                }
-                $Hso.Stop()
+
+$Hso = New-Object Net.HttpListener
+$Hso.Prefixes.Add('http://localhost:8000/')
+$Hso.Start()
+
+function GenerateResponseJson()
+{
+    $appHealthState = "Healthy"
+    $hashTable = @{
+        'ApplicationHealthState' = $appHealthState
+        'CustomMetrics' = @{
+            'RollingUpgrade' = @{
+                'PhaseOrderingNumber' = 1
+                'SkipUpgrade' = "false"
+            }
+        }
+    } 
+    $hashTable.CustomMetrics = ($hashTable.CustomMetrics | ConvertTo-Json)
+    return ($hashTable | ConvertTo-Json)
+}
+
+While($Hso.IsListening)
+{
+    $context = $Hso.GetContext()
+    $response = $context.Response
+    $response.StatusCode = 200
+    $response.ContentType = 'application/json'
+    $responseJson = GenerateResponseJson
+    $responseBytes = [System.Text.Encoding]::UTF8.GetBytes($responseJson)
+    $response.OutputStream.Write($responseBytes, 0, $responseBytes.Length)
+    $response.Close()
+}
+
+$Hso.Stop()
+
+# Echo confirmation message
+Write-Host "Script execution completed successfully."
